7a7c5f39d11763f5f6463c77d99568c01d7472d8
Catevika <dominique.bello@outlook.fr>
2022-01-28 19:09:17 +0100

    Add Register + Login routes & controllers

--- backend/server.js
+++ backend/server.js
@@ -8,4 +8,6 @@ app.get('/', (req, res) => {
 	res.status(200).json({ message: 'Welcome to the Support Desk API' });
 });
 
+app.use('/api/users', require('./routes/userRoutes'));
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
