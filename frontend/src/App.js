a43b9e623931405822daaa104a4ebc42514df810
Catevika <dominique.bello@outlook.fr>
2022-01-31 18:32:18 +0100

    Add Home, Register & Login UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
@@ -17,6 +19,7 @@ function App() {
 					</Routes>
 				</div>
 			</Router>
+			<ToastContainer />
 		</>
 	);
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,68 @@
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { FaSignInAlt, FaUser } from 'react-icons/fa';
+
 function Login() {
+	const [formData, setFormData] = useState({
+		email: '',
+		password: ''
+	});
+
+	const { email, password } = formData;
+
+	const onChange = (e) => {
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value
+		}));
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+	};
+
 	return (
-		<div>
-			<h1>Login</h1>
-		</div>
+		<>
+			<section className='heading'>
+				<h1>
+					<FaUser /> Login
+				</h1>
+				<p>Please log in to get support</p>
+			</section>
+			<section className='form'>
+				<form onSubmit={onSubmit}>
+					<div className='form-group'>
+						<input
+							type='email'
+							className='form-control'
+							id='email'
+							name='email'
+							value={email}
+							onChange={onChange}
+							placeholder='Enter your email'
+							autoComplete='username'
+							required
+						/>
+					</div>
+					<div className='form-group'>
+						<input
+							type='password'
+							className='form-control'
+							id='password'
+							name='password'
+							value={password}
+							onChange={onChange}
+							placeholder='Enter your password'
+							autoComplete='current-password'
+							required
+						/>
+					</div>
+					<div className='form-group'>
+						<button className='btn btn-block'>Submit</button>
+					</div>
+				</form>
+			</section>
+		</>
 	);
 }
 
