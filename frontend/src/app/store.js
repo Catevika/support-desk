397fec8f1c35d995c1447cc4373e6ed53468f7e7
Catevika <dominique.bello@outlook.fr>
2022-02-02 19:32:22 +0100

    Add Tickets to Redux

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice.js';
+import ticketReducer from '../features/tickets/ticketSlice.js';
 
 export const store = configureStore({
 	reducer: {
-		auth: authReducer
+		auth: authReducer,
+		ticket: ticketReducer
 	}
 });
