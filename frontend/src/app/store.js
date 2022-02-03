d4bb2e689ffe182ccec9ba626fa3db0687b5750a
Catevika <dominique.bello@outlook.fr>
2022-02-03 19:10:48 +0100

    Add Notes to Redux

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice.js';
 import ticketReducer from '../features/tickets/ticketSlice.js';
+import noteReducer from '../features/notes/noteSlice.js';
 
 export const store = configureStore({
 	reducer: {
 		auth: authReducer,
-		tickets: ticketReducer
+		tickets: ticketReducer,
+		notes: noteReducer
 	}
 });
