44ed50af741d2a2a625fd2be8e4af6746a980826
Catevika <dominique.bello@outlook.fr>
2022-02-06 13:13:16 +0100

    Heroku deployment config

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const express = require('express');
-const colors = require('colors');
 const dotenv = require('dotenv').config();
+const colors = require('colors');
 const { errorHandler } = require('./middleware/errorMiddleware');
+
 const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
@@ -14,12 +16,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-	res.status(200).json({ message: 'Welcome to the Support Desk API' });
-});
-
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+// * Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+	// * Set build folder as static
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
+	app.get('*', (_, res) => {
+		res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+	});
+} else {
+	app.get('/', (req, res) => {
+		res.status(200).json({ message: 'Welcome to the Support Desk API' });
+	});
+}
 
 app.use(errorHandler);
 
